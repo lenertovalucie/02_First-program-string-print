1f76e1df330bd59bd88ce492fc608e10c1034a69
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-07-09 09:28:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+
+
+
+##### Úkol - Prozkoumej
+
+# Jaká část programu na řádku 1 je řetězec?
+ # Answer: 
+
+# Co bude výstupem programu na řádku 1?
+ # Answer: 
+
+# Co by se stalo, pokud bych v programu na řádku 1 smazala závorku na konci?
+ # Answer: 
+
+##### Úkol - Přidej svůj kód!
+
+# Na řádky 2,3,4 přidej svůj vlastní kód! 
+# Pomocí print příkazu vytiskni do konzole své jméno, věk , záliby... Je to na tobě!
+
+##### Úkol - Výzva!
+
+# Nepovinný úkol - Příjdeš na to, jak pomocí příkazu print vytisknout prázdný řádek?
+
+##### Časté chyby
